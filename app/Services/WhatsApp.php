8c0ce6df8437fa7e1898304e28a9a984c585e55a
GabrielPauloMai <gabriel.mai@catolicasc.edu.br>
2023-04-05 15:52:45 +0000

    fix whatsapp

--- app/Services/WhatsApp.php
+++ app/Services/WhatsApp.php
@@ -7,8 +7,8 @@ use DB;
 
 class WhatsApp
 {
-    public $url = 'http://host.docker.internal:3000';
-    public function whatsapp($number, $text)
+    public $url = 'http://172.17.0.1:3000';
+   public function whatsapp($number, $text)
     {
 
 
@@ -106,29 +106,4 @@ class WhatsApp
         return $response;
     }
 
-    public function week()
-    {
-        $week = DB::select("SELECT 
-        date_format(parties.date_init,'%d/%m/%y') as 'date',
-        date_format(parties.date_init,'%H:%i') as 'hour_init',
-        clients.name as 'client_name',
-        parties.description,
-        types_events.name
-        
-        FROM EVENT
-          INNER JOIN clients on clients.id = event.client_id
-          inner join parties on event.party_id = parties.id
-          inner join types_events on parties.type = types_events.id
-          WHERE DATE_INIT < CURDATE() + INTERVAL 360 DAY
-              AND DATE_INIT > CURDATE()
-              AND event.STATUS <> 3");
-        if ($week) {
-            $text = "*FESTAS DA SEMANA*";
-            foreach ($week as $key) {
-                $text = $text."\n\n"; 
-                $text = $text."*Cliente:* $key->client_name\n*Festa:* $key->name\n*Data:* $key->date\n*Horário de Início:* $key->hour_init";
-            }
-        }
-        $this->whatsapp('4792627617', $text);
-    }
 }
